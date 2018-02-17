7d938247a8968f2f5c950b27054ab00720de5ddf
Javi Hurtado <webmaster@javi-h.com>
2018-02-17 20:59:14 +0100

    Añadir DLL de nuevo

--- Academy/Academy/FormModify.Designer.vb
+++ Academy/Academy/FormModify.Designer.vb
@@ -28,13 +28,13 @@ Partial Class FormModify
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox5 = New System.Windows.Forms.MaskedTextBox()
-        Me.MaskedTextBox6 = New System.Windows.Forms.MaskedTextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.mtbNombre = New System.Windows.Forms.MaskedTextBox()
+        Me.mtbApellido = New System.Windows.Forms.MaskedTextBox()
+        Me.mtbTel = New System.Windows.Forms.MaskedTextBox()
+        Me.mtbEmail = New System.Windows.Forms.MaskedTextBox()
+        Me.mtbDireccion = New System.Windows.Forms.MaskedTextBox()
+        Me.bDone = New System.Windows.Forms.Button()
+        Me.cbIdioma = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'Label1
@@ -42,9 +42,9 @@ Partial Class FormModify
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.SteelBlue
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(160, 117)
+        Me.Label1.Location = New System.Drawing.Point(180, 146)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(115, 32)
+        Me.Label1.Size = New System.Drawing.Size(133, 38)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Nombre"
         '
@@ -52,9 +52,9 @@ Partial Class FormModify
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(160, 172)
+        Me.Label2.Location = New System.Drawing.Point(180, 215)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(119, 32)
+        Me.Label2.Size = New System.Drawing.Size(132, 38)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Apellido"
         '
@@ -62,9 +62,9 @@ Partial Class FormModify
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(160, 215)
+        Me.Label3.Location = New System.Drawing.Point(180, 269)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(127, 32)
+        Me.Label3.Size = New System.Drawing.Size(143, 38)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Teléfono"
         '
@@ -72,9 +72,9 @@ Partial Class FormModify
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(160, 269)
+        Me.Label4.Location = New System.Drawing.Point(180, 336)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(247, 32)
+        Me.Label4.Size = New System.Drawing.Size(284, 38)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Correo electrónico"
         '
@@ -82,9 +82,9 @@ Partial Class FormModify
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(160, 332)
+        Me.Label5.Location = New System.Drawing.Point(180, 415)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(134, 32)
+        Me.Label5.Size = New System.Drawing.Size(154, 38)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "Dirección"
         '
@@ -92,89 +92,97 @@ Partial Class FormModify
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(160, 377)
+        Me.Label6.Location = New System.Drawing.Point(180, 471)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(100, 32)
+        Me.Label6.Size = New System.Drawing.Size(113, 38)
         Me.Label6.TabIndex = 5
         Me.Label6.Text = "Idioma"
         '
-        'MaskedTextBox1
+        'mtbNombre
         '
-        Me.MaskedTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaskedTextBox1.Location = New System.Drawing.Point(420, 117)
-        Me.MaskedTextBox1.Name = "MaskedTextBox1"
-        Me.MaskedTextBox1.Size = New System.Drawing.Size(356, 38)
-        Me.MaskedTextBox1.TabIndex = 6
+        Me.mtbNombre.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mtbNombre.Location = New System.Drawing.Point(472, 146)
+        Me.mtbNombre.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.mtbNombre.Name = "mtbNombre"
+        Me.mtbNombre.Size = New System.Drawing.Size(400, 44)
+        Me.mtbNombre.TabIndex = 6
         '
-        'MaskedTextBox2
+        'mtbApellido
         '
-        Me.MaskedTextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaskedTextBox2.Location = New System.Drawing.Point(420, 172)
-        Me.MaskedTextBox2.Name = "MaskedTextBox2"
-        Me.MaskedTextBox2.Size = New System.Drawing.Size(356, 38)
-        Me.MaskedTextBox2.TabIndex = 7
+        Me.mtbApellido.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mtbApellido.Location = New System.Drawing.Point(472, 215)
+        Me.mtbApellido.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.mtbApellido.Name = "mtbApellido"
+        Me.mtbApellido.Size = New System.Drawing.Size(400, 44)
+        Me.mtbApellido.TabIndex = 7
         '
-        'MaskedTextBox3
+        'mtbTel
         '
-        Me.MaskedTextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaskedTextBox3.Location = New System.Drawing.Point(420, 215)
-        Me.MaskedTextBox3.Name = "MaskedTextBox3"
-        Me.MaskedTextBox3.Size = New System.Drawing.Size(356, 38)
-        Me.MaskedTextBox3.TabIndex = 8
+        Me.mtbTel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mtbTel.Location = New System.Drawing.Point(472, 269)
+        Me.mtbTel.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.mtbTel.Name = "mtbTel"
+        Me.mtbTel.Size = New System.Drawing.Size(400, 44)
+        Me.mtbTel.TabIndex = 8
         '
-        'MaskedTextBox4
+        'mtbEmail
         '
-        Me.MaskedTextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaskedTextBox4.Location = New System.Drawing.Point(420, 264)
-        Me.MaskedTextBox4.Name = "MaskedTextBox4"
-        Me.MaskedTextBox4.Size = New System.Drawing.Size(356, 38)
-        Me.MaskedTextBox4.TabIndex = 9
+        Me.mtbEmail.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mtbEmail.Location = New System.Drawing.Point(472, 330)
+        Me.mtbEmail.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.mtbEmail.Name = "mtbEmail"
+        Me.mtbEmail.Size = New System.Drawing.Size(400, 44)
+        Me.mtbEmail.TabIndex = 9
         '
-        'MaskedTextBox5
+        'mtbDireccion
         '
-        Me.MaskedTextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaskedTextBox5.Location = New System.Drawing.Point(420, 327)
-        Me.MaskedTextBox5.Name = "MaskedTextBox5"
-        Me.MaskedTextBox5.Size = New System.Drawing.Size(356, 38)
-        Me.MaskedTextBox5.TabIndex = 10
+        Me.mtbDireccion.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mtbDireccion.Location = New System.Drawing.Point(472, 409)
+        Me.mtbDireccion.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.mtbDireccion.Name = "mtbDireccion"
+        Me.mtbDireccion.Size = New System.Drawing.Size(400, 44)
+        Me.mtbDireccion.TabIndex = 10
         '
-        'MaskedTextBox6
+        'bDone
         '
-        Me.MaskedTextBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaskedTextBox6.Location = New System.Drawing.Point(420, 377)
-        Me.MaskedTextBox6.Name = "MaskedTextBox6"
-        Me.MaskedTextBox6.Size = New System.Drawing.Size(356, 38)
-        Me.MaskedTextBox6.TabIndex = 11
+        Me.bDone.Font = New System.Drawing.Font("Modern No. 20", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bDone.Location = New System.Drawing.Point(457, 644)
+        Me.bDone.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.bDone.Name = "bDone"
+        Me.bDone.Size = New System.Drawing.Size(181, 76)
+        Me.bDone.TabIndex = 12
+        Me.bDone.Text = "Modificar"
+        Me.bDone.UseVisualStyleBackColor = True
         '
-        'Button1
+        'cbIdioma
         '
-        Me.Button1.Font = New System.Drawing.Font("Modern No. 20", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(406, 515)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(161, 61)
-        Me.Button1.TabIndex = 12
-        Me.Button1.Text = "Modificar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.cbIdioma.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbIdioma.FormattingEnabled = True
+        Me.cbIdioma.Location = New System.Drawing.Point(472, 471)
+        Me.cbIdioma.Name = "cbIdioma"
+        Me.cbIdioma.Size = New System.Drawing.Size(400, 45)
+        Me.cbIdioma.TabIndex = 13
         '
         'FormModify
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.SteelBlue
-        Me.ClientSize = New System.Drawing.Size(1039, 729)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.MaskedTextBox6)
-        Me.Controls.Add(Me.MaskedTextBox5)
-        Me.Controls.Add(Me.MaskedTextBox4)
-        Me.Controls.Add(Me.MaskedTextBox3)
-        Me.Controls.Add(Me.MaskedTextBox2)
-        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.ClientSize = New System.Drawing.Size(1169, 911)
+        Me.Controls.Add(Me.cbIdioma)
+        Me.Controls.Add(Me.bDone)
+        Me.Controls.Add(Me.mtbDireccion)
+        Me.Controls.Add(Me.mtbEmail)
+        Me.Controls.Add(Me.mtbTel)
+        Me.Controls.Add(Me.mtbApellido)
+        Me.Controls.Add(Me.mtbNombre)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "FormModify"
         Me.Text = "FormModify"
         Me.ResumeLayout(False)
@@ -188,11 +196,11 @@ Partial Class FormModify
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents MaskedTextBox1 As MaskedTextBox
-    Friend WithEvents MaskedTextBox2 As MaskedTextBox
-    Friend WithEvents MaskedTextBox3 As MaskedTextBox
-    Friend WithEvents MaskedTextBox4 As MaskedTextBox
-    Friend WithEvents MaskedTextBox5 As MaskedTextBox
-    Friend WithEvents MaskedTextBox6 As MaskedTextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents mtbNombre As MaskedTextBox
+    Friend WithEvents mtbApellido As MaskedTextBox
+    Friend WithEvents mtbTel As MaskedTextBox
+    Friend WithEvents mtbEmail As MaskedTextBox
+    Friend WithEvents mtbDireccion As MaskedTextBox
+    Friend WithEvents bDone As Button
+    Friend WithEvents cbIdioma As ComboBox
 End Class
--- Academy/Academy/FormModify.vb
+++ Academy/Academy/FormModify.vb
@@ -1,3 +1,9 @@
 ﻿Public Class FormModify
+    Private Sub FormModify_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub MaskedTextBox3_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles mtbTel.MaskInputRejected
+
+    End Sub
 End Class
