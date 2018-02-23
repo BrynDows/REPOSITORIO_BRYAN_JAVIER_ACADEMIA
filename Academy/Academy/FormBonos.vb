9834bccd8bff9b82c2783145a3f7c985da09940b
Javi Hurtado <webmaster@javi-h.com>
2018-02-24 00:32:30 +0100

    Precios bonos cambian

--- Academy/Academy/FormBonos.vb
+++ Academy/Academy/FormBonos.vb
@@ -3,8 +3,39 @@
 Public Class FormBonos
 
     Public Property alu As Alumno
+    Private actual As String()
+    Private isDisabled As Boolean
 
     Private Sub FormBonos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        actual = idiomasDLL.Bonos.SelectPrecio(rbInvierno.Checked, rbPresencial.Checked, rbDomi.Checked, nudHoras.Value, nudPersonas.Value)
+        lPrice.Text = actual(1) & " €"
         lTitle.Text = "Asignar bono al alumno " & alu.Nombre & " " & alu.Apellido
+    End Sub
+
+    Private Sub rbSkype_CheckedChanged(sender As Object, e As EventArgs) Handles rbSkype.CheckedChanged
+        If rbSkype.Checked Then
+            If nudPersonas.Value > 5 Then
+                nudPersonas.Value = 5
+            End If
+            nudPersonas.Maximum = 5
+            gbSeason.Enabled = False
+            gbLugar.Enabled = False
+            isDisabled = True
+        Else
+            nudPersonas.Maximum = 6
+            gbSeason.Enabled = True
+            gbLugar.Enabled = True
+            isDisabled = False
+        End If
+        UpdatePrice(sender, e)
+    End Sub
+
+    Private Sub UpdatePrice(sender As Object, e As EventArgs) Handles rbInvierno.CheckedChanged, rbEscuela.CheckedChanged, nudPersonas.ValueChanged, nudHoras.ValueChanged
+        If isDisabled Then
+            actual = idiomasDLL.Bonos.SelectPrecio(False, rbPresencial.Checked, False, nudHoras.Value, nudPersonas.Value)
+        Else
+            actual = idiomasDLL.Bonos.SelectPrecio(rbInvierno.Checked, rbPresencial.Checked, rbDomi.Checked, nudHoras.Value, nudPersonas.Value)
+        End If
+        lPrice.Text = actual(1) & " €"
     End Sub
 End Class
--- idiomasDLL/idiomasDLL/Bonos.vb
+++ idiomasDLL/idiomasDLL/Bonos.vb
@@ -3,6 +3,22 @@
 Public Class Bonos
     Private Shared ReadOnly Property connection As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=academy_bd.accdb")
 
+    Public Shared Function SelectPrecio(epoca As Boolean, presencial As Boolean, lugar As Boolean, horas As Int16, personas As Int16) As String()
+        Dim adapter As New OleDbDataAdapter("SELECT id, precio FROM bonos WHERE horas = " & horas &
+                                            " AND presencial = " & presencial &
+                                            " AND n_personas = " & personas &
+                                            " AND domiCuela = " & lugar &
+                                            " AND invierano = " & epoca, connection)
+        Dim midataset As New DataSet
+        adapter.Fill(midataset, "precio")
+        Dim ret(2) As String
+        If midataset.Tables(0).Rows.Count > 0 Then
+            ret(0) = midataset.Tables(0).Rows(0)("id").ToString
+            ret(1) = midataset.Tables(0).Rows(0)("precio").ToString
+        End If
+        Return ret
+    End Function
+
     Public Shared Sub CloseConnection()
         connection.Close()
     End Sub
