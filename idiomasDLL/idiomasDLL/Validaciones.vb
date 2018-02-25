1a97e427902019f4239e3fb376aaa5f437fe0b2a
Javi Hurtado <webmaster@javi-h.com>
2018-02-25 12:52:08 +0100

    Validaciones

--- idiomasDLL/idiomasDLL/Validaciones.vb
+++ idiomasDLL/idiomasDLL/Validaciones.vb
@@ -5,4 +5,12 @@ Public Module Validaciones
         Return Regex.IsMatch(email, "^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]" &
         "*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$")
     End Function
+
+    Public Function isValidName(ByVal name As String) As Boolean
+        Return Regex.IsMatch(name, "^[a-zA-Z]{1,20}$")
+    End Function
+
+    Public Function isValidApe(ByVal ape As String) As Boolean
+        Return Regex.IsMatch(ape, "^[a-zA-Z]{1,50}$")
+    End Function
 End Module
